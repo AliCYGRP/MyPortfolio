c53692a72cf12d8f26c5a772a5cf6b08439ea0b7
Syed Ali Hasan <syed.hasan@cybergindia.local>
2021-04-14 21:33:20 +0530

    Added JS. Functional buttons

---
+++ script/script.js
@@ -0,0 +1,37 @@
+function openFB() {
+  window.open("https://www.facebook.com/Zyro9922/", "_blank");
+}
+
+function openInsta() {
+  window.open("http://instagram.com/alihasan9922", "_blank");
+}
+
+function openLinkedin() {
+  window.open("https://www.linkedin.com/in/Zyro9922", "_blank");
+}
+
+function openGithub() {
+  window.open("https://github.com/AliCYGRP/", "_blank");
+}
+
+function myfunction1() {
+  window.open("https://www.twitter.com/initishsharmaa", "_blank");
+}
+
+function myfunction2() {
+  var email = "sharma.nitish72@gmail.com";
+  var mailto_link = "mailto:" + email;
+  window = window.open(mailto_link, "emailWindow");
+  if (window && window.open && !window.closed) window.close();
+}
+
+function myfunction3() {
+  window.open("https://www.linkedin.com/in/nitish-sharma-226420132/", "_blank");
+}
+
+function myfunction4() {
+  var tel = "+91-7456031322";
+  var tel_link = "tel:" + tel;
+  window = window.open(tel_link, "emailWindow");
+  if (window && window.open && !window.closed) window.close();
+}
